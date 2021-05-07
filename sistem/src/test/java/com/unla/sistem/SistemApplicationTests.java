5f77fcb2c53283d44c96f8e9f6fa40dbc2d1e1f2
USUARIO-PC\USUARIO <oscarruina@hotmail.com>
2021-05-07 12:48:30 -0300

    added spring files

---
+++ sistem/src/test/java/com/unla/sistem/SistemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unla.sistem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
