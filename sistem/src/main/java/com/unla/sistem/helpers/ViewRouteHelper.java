7f6a1be7eb83c4dfce431428ecc2d81a79cf8e32
USUARIO-PC\USUARIO <oscarruina@hotmail.com>
2021-05-10 13:20:18 -0300

    add packages and flogin controller
    pending the views login, logout and permission from users

---
+++ sistem/src/main/java/com/unla/sistem/helpers/ViewRouteHelper.java
@@ -0,0 +1,9 @@
+package com.unla.sistem.helpers;
+
+public class ViewRouteHelper {
+	
+	//user
+	public final static String USER_LOGIN = "user/login";
+	public final static String USER_LOGOUT = "user/logout";
+
+}
